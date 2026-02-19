958d286c35c8f22249840024d1f95b11351e0b19
Pintos Machado Lara Aylen <larapintos.61@gmail.com>
2026-02-18 21:56:58 -0300

    Initial commit

---
+++ IntroduccionJava/src/Saludos.java
@@ -0,0 +1,7 @@
+public class Saludos {
+    public static void main(String[] args) {
+        System.out.println("Juan");
+        System.out.println("65");
+        System.out.println("Holanda");
+    }
+}
