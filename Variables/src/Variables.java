434754e1a17d71b23e1656aaae074b479978fe98
Pintos Machado Lara Aylen <larapintos.61@gmail.com>
2026-02-18 22:05:21 -0300

    variables

---
+++ Variables/src/Variables.java
@@ -0,0 +1,22 @@
+public class Variables {
+    //Variables
+    public static void main(String[] args) {
+        int edad = 30;
+        double sueldo = 203300.50;
+        String nombre = "Karla";
+
+        //Acceder a los valores
+
+        System.out.println(edad);
+        System.out.println(sueldo);
+        System.out.println(nombre);
+
+        //Modificar el valor de las variables
+
+        edad = 35;
+
+        System.out.println("Edad modificada: "+edad);
+    }
+
+
+}
