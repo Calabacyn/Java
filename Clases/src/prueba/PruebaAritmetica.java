d9641f5c2f05e37d281d6eda9c4fffbe41216581
Pintos Machado Lara Aylen <larapintos.61@gmail.com>
2026-03-04 15:46:44 -0300

    encapsulamiento

---
+++ Clases/src/prueba/PruebaAritmetica.java
@@ -0,0 +1,18 @@
+package prueba;
+
+import aritmetica.Aritmetica;
+
+public class PruebaAritmetica {
+
+    public static void main(String[] args) {
+        System.out.println("*** Ejemplo aritmetica.Aritmetica ***");
+        var aritmetica1 = new Aritmetica( 7, 8);
+        System.out.println("aritmetica1.getOperando1() = " + aritmetica1.getOperando1());
+      aritmetica1.setOperando1(9);
+        System.out.println("aritmetica1.getOperando1() = " + aritmetica1.getOperando1());
+    aritmetica1.sumar();
+
+
+
+    }
+}
