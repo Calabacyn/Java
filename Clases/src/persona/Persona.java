766aabd3617d3dd81f9ce8426860561ce01d182f
Pintos Machado Lara Aylen <larapintos.61@gmail.com>
2026-03-08 18:27:13 -0300

    Metodos Estaticos

--- Clases/src/persona/Persona.java
+++ Clases/src/persona/Persona.java
@@ -2,17 +2,28 @@ package persona;
 
 public class Persona {
 
+    private static int contadorPersonas=0;
     private String nombre;
     private String apellido;
+    private int idPersona;
 
     // Contrutor
 
     public Persona( String nombre, String apellido){
         this.nombre = nombre;
         this.apellido = apellido;
+
+        this.idPersona = ++Persona.contadorPersonas;
     }
 
+    @Override
+    public String toString(){
+        return  "ID: " + this.idPersona
+                + ", Nombre: " + this.nombre
+                + ", apellido: " + this.apellido;
+    }
     void mostrarPersona(){
+
         System.out.println("Nombre: " + nombre);
         System.out.println("Apellido: " + apellido);
     }
@@ -31,5 +42,17 @@ public class Persona {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public  static int getContadorPersonas(){
+        return  Persona.contadorPersonas;
     }
 }
--- Clases/src/persona/PruebaPersona.java
+++ Clases/src/persona/PruebaPersona.java
@@ -4,8 +4,17 @@ public class PruebaPersona {
 
     public static void main(String[] args) {
         System.out.println("Creacion de Objetos de la Clase persona.Persona");
-        Persona objeto1 = new Persona( "Maria", "Lopez");
+
         System.out.println("****************************************************");
-        objeto1.mostrarPersona();
+
+
+        Persona objeto1 = new Persona( "Maria", "Lopez");
+        System.out.println(objeto1.toString());
+
+        Persona objeto2 = new Persona( "Juan", "Lopez");
+        System.out.println(objeto2.toString());
+
+        System.out.println("Variable estatica: "+Persona.getContadorPersonas());
+
     }
 }
